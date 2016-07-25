662c03126cc329fc588fca052b0d61371345db7e
georgecode <georgeczurlanis@gmail.com>
2016-07-25 18:52:50 -0400

    Final oval calc

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
-console.log("hello js works");
 
-$(".hello" ).click(function() {
-  console.log("hello");
-  var info = document.getElementById('words').value
-  console.log(info);
-});
+$(".btn" ).click(function() {
 
-var a = 400;//bigger
-var b = 300;//smaller
+
+	var a = document.getElementById('diamA').value;
+	var b = document.getElementById('diamB').value;
 
 //The famous Indian mathematician Ramanujan 
 //came up with this better approximation below:
@@ -19,47 +15,52 @@ function oval(a,b){
 
 }
 */
-function oval(a,b){
-	var result = 2*Math.PI*Math.sqrt((Math.pow(a, 2)+Math.pow(b, 2))/2)
-	console.log(result);
-	return result;
+	function oval(a,b){
+		a=a/2;
+		b=b/2;
+		var result = 2*Math.PI*Math.sqrt((Math.pow(a, 2)+Math.pow(b, 2))/2)
+		console.log(result);
+		return result;
 
-}
+	}
+
+
 
 //Must be a number
-function isANumber(a,b){
-	if(typeof a === 'number' && typeof b === 'number'){
-		return true
-	}else if(typeof a !== 'number' && typeof b !== 'number'){
-		console.log("a & b are not number's");
-	}else if(typeof a !== 'number' && typeof b === 'number'){
-		console.log("a is not a number");
-	}else{
-		console.log("a is not a number");
+	function isANumber(a,b){
+		if(typeof a === 'number' && typeof b === 'number'){
+			return true
+		}else if(typeof a !== 'number' && typeof b !== 'number'){
+			console.log("a & b are not number's");
+		}else if(typeof a !== 'number' && typeof b === 'number'){
+			console.log("a is not a number");
+		}else{
+			console.log("a is not a number");
+		}
 	}
-}
 //result is a number
-function resultNum(a,b){
-	let test= oval(a,b);
-	if(typeof test === 'number'){
-		return true
-	}else{
-		console.log("result is not outputing a number");
+	function resultNum(a,b){
+		let test= oval(a,b);
+		if(typeof test === 'number'){
+			return true
+		}else{
+			console.log("result is not outputing a number");
+		}
 	}
-}
 
 //is equal to function
 
 
-
 //TESTS RUNNING
-isANumber(a,b);
-resultNum(a,b);
+	isANumber(a,b);
+	resultNum(a,b);
 
 //Results
-oval(a,b);
+	var result= oval(a,b);
+	console.log(result);
+ 	$(".results").html(result);
 
-
+});
 
 
 
